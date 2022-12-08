9d7c62d94c7c27077f6e1047449e99b79f92df48
cApuer4uk <heroes4capu@gmail.com>
2022-12-08 11:26:12 +0300

    инструкция создания гита

--- Task1.py
+++ Task1.py
@@ -1,1 +1,7 @@
-a = int(input())
+git init
+  git status
+  git remote and origin https/...
+  git add
+  git config --global user.name "name"
+  git config --global user.email "@gmail.com"
+  git commit -m "аннотация"
